5b8a9d5092629b1ebd437b38892fbbce7aaca5e6
KesvaL <47763141+xKesvaL@users.noreply.github.com>
2023-10-04 13:02:38 +0000

    fix: use langStore in hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,16 @@
 import type { Handle } from '@sveltejs/kit';
 
-import { locale } from 'svelte-i18n';
+import { langStore } from '$lib/stores/lang';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	let lang =
 		event.url.searchParams.get('lang') ||
 		event.request.headers.get('accept-language')?.split(',')[0];
+
 	lang = lang?.split('-')[0];
+
 	if (lang) {
-		locale.set(lang);
+		langStore.set(lang);
 	}
 	return resolve(event);
 };
